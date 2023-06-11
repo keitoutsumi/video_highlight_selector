4fe39cff96711e759178a79d91912bf77653d3a3
keitoutsumi <90272383+keitoutsumi@users.noreply.github.com>
2023-06-11 19:28:39 +0900

    Update video_understanding.py

--- video_understanding.py
+++ video_understanding.py
@@ -1,6 +1,6 @@
 import torch
 from torchvision.models.video import r3d_18
-
+#video understanding model to extract video features
 def load_slowfast_model():
     model = r3d_18(pretrained=True)
     model = model.cuda()
